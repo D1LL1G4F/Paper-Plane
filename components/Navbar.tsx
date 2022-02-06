b9085acf33ef6ac904e8aae5b2841d13c9062fab
matej.knazik <matej.knazik@kiwi.com>
2022-02-06 21:52:43 +0100

    feat(ui): create landing page

---
+++ components/Navbar.tsx
@@ -0,0 +1,62 @@
+import { ReactElement } from "react";
+import transition from "@kiwicom/orbit-components/lib/utils/transition";
+import mq from "@kiwicom/orbit-components/lib/utils/mediaQuery";
+import LinkList from "@kiwicom/orbit-components/lib/LinkList";
+import TextLink from "@kiwicom/orbit-components/lib/TextLink";
+import Stack from "@kiwicom/orbit-components/lib/Stack";
+import GitHubLogo from "/public/GitHub-Logo-32x32.png";
+import PaperPlaneLogo from "/public/PaperPlaneLogo640x464.png";
+import styled, { css } from "styled-components";
+import Image from "next/image";
+
+const StyledNavigationBar = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 52px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  background: ${({ theme }) => theme.orbit.paletteProductDark};
+  box-shadow: ${({ theme }) => theme.orbit.boxShadowFixed};
+  padding: ${({ theme }) => theme.orbit.spaceSmall};
+  box-sizing: border-box;
+  z-index: 700;
+  transition: ${transition(["transform"], "normal", "ease-in-out")};
+  ${mq.tablet(css`
+    height: 64px;
+  `)};
+`;
+
+const Navbar = (): ReactElement => (
+  <StyledNavigationBar>
+    <Stack direction="row" align="center" justify="center">
+      <TextLink href="/">
+        <Image
+          src={PaperPlaneLogo}
+          width={80}
+          height={58}
+          alt="Paper Plane Logo"
+        />
+      </TextLink>
+      <LinkList direction="row">
+        <TextLink type="white" href="/">
+          Projects
+        </TextLink>
+      </LinkList>
+      <Stack inline>
+        <LinkList direction="row">
+          <TextLink
+            type="secondary"
+            href="https://github.com/D1LL1G4F/Paper-Plane"
+          >
+            <Image src={GitHubLogo} alt="GitHub" />
+          </TextLink>
+        </LinkList>
+      </Stack>
+    </Stack>
+  </StyledNavigationBar>
+);
+
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,32 @@
 import { ReactElement } from "react";
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import styled from "styled-components";
+import Grid from "@kiwicom/orbit-components/lib/utils/Grid";
+import Navbar from "../components/Navbar";
+import ThemeProvider from "@kiwicom/orbit-components/lib/ThemeProvider";
+import { defaultTheme } from "@kiwicom/orbit-components";
+import "normalize.css/normalize.css";
+
+const NavContainer = styled.header`
+  z-index: 99;
+  position: relative;
+`;
+
+const GridWrapper = styled(Grid)`
+  min-height: 100vh;
+`;
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GridWrapper rows="64px 1fr">
+        <NavContainer>
+          <Navbar />
+        </NavContainer>
+        <Component {...pageProps} />
+      </GridWrapper>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
