4c4cf8eb19d0a648d17a7d7f677aec15119a54ae
matej.knazik <matej.knazik@kiwi.com>
2022-02-06 13:55:31 +0100

    chore(eslint): setup linters

--- pages/api/[[...route]].ts
+++ pages/api/[[...route]].ts
@@ -1,18 +1,24 @@
+/* eslint-disable babel/camelcase */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-    name: string,
-    mocked_route: string,
-    method: string | undefined,
-    request_body: any
-}
+  name: string;
+  mocked_route: string;
+  method: string | undefined;
+  request_body: unknown;
+};
 
 export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
-) {
-    const { route } = req.query
-    const mockedRoute = Array.isArray(route) ? route.join('/') : route
-    res.status(200).json({ name: 'Mocked API response', mocked_route: mockedRoute, method: req.method, request_body: req.body })
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+): void {
+  const { route } = req.query;
+  const mockedRoute = Array.isArray(route) ? route.join("/") : route;
+  res.status(200).json({
+    name: "Mocked API response",
+    mocked_route: mockedRoute,
+    method: req.method,
+    request_body: req.body,
+  });
 }
