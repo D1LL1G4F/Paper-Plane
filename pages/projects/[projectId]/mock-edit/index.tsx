681a5df1d688ffa1473c10052b918cf971a8d773
matej.knazik <matej.knazik@kiwi.com>
2022-03-06 23:10:35 +0100

    feat(ui): edit mock page prototype

--- pages/projects/[projectId]/mock-edit/index.tsx
+++ pages/projects/[projectId]/mock-edit/index.tsx
@@ -5,17 +5,30 @@ import Layout from "../../../../components/Layout";
 import Box from "@kiwicom/orbit-components/lib/Box";
 import Image from "next/image";
 import PaperPlaneLogo from "/public/PaperPlaneLogo1280x929.png";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
+import InputField from "@kiwicom/orbit-components/lib/InputField";
+import { Separator } from "@kiwicom/orbit-components";
+import Button from "@kiwicom/orbit-components/lib/Button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
 type MockEditForm = {
-  firstName: string;
-  lastName: string;
+  mockName: string;
+  mockDescription: string;
+  urlPath: string;
 };
 
+const schema = z.object({
+  mockName: z.string().min(1, { message: "Required" }),
+  mockDescription: z.string().min(1, { message: "Required" }),
+  urlPath: z.string().optional(),
+});
+
 const MockEdit: NextPage = () => {
-  const { register, setValue, handleSubmit } = useForm<MockEditForm>();
-  // eslint-disable-next-line no-console
-  const onSubmit = handleSubmit((data) => console.log(data));
+  const { handleSubmit, watch, control } = useForm<MockEditForm>({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit = handleSubmit((data) => data);
 
   return (
     <Layout
@@ -23,8 +36,12 @@ const MockEdit: NextPage = () => {
         <Stack justify="start" direction="column" align="center">
           <Box padding="XLarge">
             <Stack direction="column" spacing="large">
-              <Heading type="display">New Mock</Heading>
-              <Heading type="displaySubtitle">Mock description...</Heading>
+              <Heading type="display">
+                {watch("mockName") || "New Mock"}
+              </Heading>
+              <Heading type="displaySubtitle">
+                {watch("mockDescription") || "Mock description..."}
+              </Heading>
               <Stack align="center" justify="center">
                 <Box maxWidth="90vh">
                   <Image src={PaperPlaneLogo} alt="Paper Plane Logo" />
@@ -36,18 +53,55 @@ const MockEdit: NextPage = () => {
       }
     >
       <form onSubmit={onSubmit}>
-        <label>First Name</label>
-        <input {...register("firstName")} />
-        <label>Last Name</label>
-        <input {...register("lastName")} />
-        <button
-          type="button"
-          onClick={() => {
-            setValue("lastName", "luo"); // ✅
-          }}
-        >
-          SetValue
-        </button>
+        <Stack spacing="XLarge">
+          <Stack>
+            <Controller
+              name="mockName"
+              control={control}
+              render={({ field, fieldState }) => (
+                <InputField
+                  {...field}
+                  label="Mock Name"
+                  placeholder="New Mock"
+                  error={fieldState.error?.message}
+                />
+              )}
+            />
+            <Controller
+              name="mockDescription"
+              control={control}
+              render={({ field, fieldState }) => (
+                <InputField
+                  {...field}
+                  label="Mock Description"
+                  placeholder="Mock description..."
+                  error={fieldState.error?.message}
+                />
+              )}
+            />
+          </Stack>
+          <Separator />
+          <Controller
+            name="urlPath"
+            control={control}
+            render={({ field, fieldState }) => (
+              <InputField
+                {...field}
+                label="Website URL path"
+                placeholder="/custom-path"
+                prefix="https://kiwi.com"
+                error={fieldState.error?.message}
+              />
+            )}
+          />
+          <Separator />
+
+          <Stack direction="row-reverse">
+            <Button size="large" submit>
+              Submit
+            </Button>
+          </Stack>
+        </Stack>
       </form>
     </Layout>
   );
