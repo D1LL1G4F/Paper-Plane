e678a387aedf0f8ebd4089547f52376270fc3e09
matej.knazik <matej.knazik@kiwi.com>
2022-04-15 15:19:58 +0200

    feat(layout): add loading page layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Grid from "@kiwicom/orbit-components/lib/utils/Grid";
+import Stack from "@kiwicom/orbit-components/lib/Stack";
+import { Box, Skeleton, Loading } from "@kiwicom/orbit-components";
 
 const PageGrid = styled(Grid)`
   width: 100%;
@@ -21,13 +23,37 @@ const layoutOptions = {
 type LayoutProps = {
   children: JSX.Element;
   sidebar: JSX.Element;
+  isLoading?: boolean;
 };
 
-const Layout = ({ sidebar, children }: LayoutProps): JSX.Element => {
+const Layout = ({ sidebar, children, isLoading }: LayoutProps): JSX.Element => {
   return (
     <PageGrid {...layoutOptions}>
-      <Grid rows="1fr auto auto">{sidebar}</Grid>
-      <PageContent>{children}</PageContent>
+      <Grid rows="1fr auto auto">
+        {isLoading ? (
+          <Stack justify="start" direction="column" align="center">
+            <Box padding="XLarge" width="100%">
+              <Stack direction="column" spacing="XXLarge">
+                <Skeleton height={70} width={300} />
+                <Skeleton height={60} width={400} />
+              </Stack>
+            </Box>
+          </Stack>
+        ) : (
+          sidebar
+        )}
+      </Grid>
+      <PageContent>
+        {isLoading ? (
+          <Stack justify="center" direction="column" align="center">
+            <Box padding="XLarge" width="100%">
+              <Loading type="pageLoader" />
+            </Box>
+          </Stack>
+        ) : (
+          children
+        )}
+      </PageContent>
     </PageGrid>
   );
 };
